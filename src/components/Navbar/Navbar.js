e5fc1fa911878e7bba7549c7a9940be3c94c8be3
Olivia Adongo <akinyioliviatove@gmail.com>
2022-09-07 10:03:32 +0300

    Components added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+    <nav className="navbar">
+    <div className="container">
+    <button>Go on</button>
+    </div>
+    </nav>
+    
+    </div>
+  )
+}
+
+export default Navbar
